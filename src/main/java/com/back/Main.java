855b31591adf6b7f575f9cfac72188f3accedfd3
jhs512 <jangka512@gmail.com>
2025-06-02 10:26:08 +0900

    lab 1-1

---
+++ src/main/java/com/back/Main.java
@@ -0,0 +1,17 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        lab1();
+    }
+
+    private static void lab1() {
+        Scanner scanner = new Scanner(System.in);
+
+        String cmd = scanner.nextLine();
+
+        System.out.println(cmd);
+    }
+}
