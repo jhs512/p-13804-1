db7fd234bf28271b1af13c3eef3c0cba332ef91f
jhs512 <jangka512@gmail.com>
2025-06-04 12:36:23 +0900

    step 17, g

--- src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -24,6 +24,7 @@ public class WiseSayingRepository {
         return wiseSayings
                 .reversed()
                 .stream()
+                .skip((pageNo - 1) * pageSize)
                 .limit(pageSize)
                 .collect(Collectors.toList());
     }
@@ -55,6 +56,7 @@ public class WiseSayingRepository {
                 .filter(
                         w -> w.getContent().contains(keyword)
                 )
+                .skip((pageNo - 1) * pageSize)
                 .limit(pageSize)
                 .collect(Collectors.toList());
     }
@@ -66,6 +68,7 @@ public class WiseSayingRepository {
                 .filter(
                         w -> w.getAuthor().contains(keyword)
                 )
+                .skip((pageNo - 1) * pageSize)
                 .limit(pageSize)
                 .collect(Collectors.toList());
     }
@@ -76,6 +79,7 @@ public class WiseSayingRepository {
                 .filter(
                         w -> w.getContent().contains(keyword1) || w.getAuthor().contains(keyword2)
                 )
+                .skip((pageNo - 1) * pageSize)
                 .limit(pageSize)
                 .collect(Collectors.toList())
                 .reversed();
