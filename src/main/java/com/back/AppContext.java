b4565c3845e26871a8a2870c15d664cbeca5381f
jhs512 <jangka512@gmail.com>
2025-06-05 09:37:05 +0900

    step 22, r

--- src/main/java/com/back/AppContext.java
+++ src/main/java/com/back/AppContext.java
@@ -2,6 +2,7 @@ package com.back;
 
 import com.back.domain.system.controller.SystemController;
 import com.back.domain.wiseSaying.controller.WiseSayingController;
+import com.back.domain.wiseSaying.repository.WiseSayingFileRepository;
 import com.back.domain.wiseSaying.repository.WiseSayingRepository;
 import com.back.domain.wiseSaying.service.WiseSayingService;
 
@@ -10,6 +11,7 @@ import java.util.Scanner;
 public class AppContext {
     public static Scanner scanner;
     public static SystemController systemController;
+    public static WiseSayingFileRepository wiseSayingFileRepository;
     public static WiseSayingRepository wiseSayingRepository;
     public static WiseSayingService wiseSayingService;
     public static WiseSayingController wiseSayingController;
@@ -17,6 +19,7 @@ public class AppContext {
     public static void renew(Scanner _scanner) {
         scanner = _scanner;
         systemController = new SystemController();
+        wiseSayingFileRepository = new WiseSayingFileRepository();
         wiseSayingRepository = new WiseSayingRepository();
         wiseSayingService = new WiseSayingService();
         wiseSayingController = new WiseSayingController();
---
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepository.java
@@ -0,0 +1,12 @@
+package com.back.domain.wiseSaying.repository;
+
+import com.back.domain.wiseSaying.entity.WiseSaying;
+
+public class WiseSayingFileRepository {
+    public void save(WiseSaying wiseSaying) {
+    }
+
+    public WiseSaying findById(int id) {
+        return null;
+    }
+}
