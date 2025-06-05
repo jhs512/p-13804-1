d850622f9d0d1f7b1d76cc417ee62b8975311667
jhs512 <jangka512@gmail.com>
2025-06-05 09:50:18 +0900

    step 22, g

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -4,6 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
@@ -17,7 +21,35 @@ public class WiseSaying {
         this.author = author;
     }
 
+    public WiseSaying(Map<String, Object> map) {
+        this.id = (int) map.get("id");
+        this.content = (String) map.get("content");
+        this.author = (String) map.get("author");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        WiseSaying that = (WiseSaying) o;
+        return id == that.id && Objects.equals(content, that.content) && Objects.equals(author, that.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content, author);
+    }
+
     public boolean isNew() {
         return id == 0;
+    }
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new LinkedHashMap<>();
+
+        map.put("id", id);
+        map.put("content", content);
+        map.put("author", author);
+
+        return map;
     }
 }
